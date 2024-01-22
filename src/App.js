ac4cace01e1a3a44fc9b25e1d81cb85881da039b
Tuhin Das <dastuhin.rnj@gmail.com>
2024-01-22 20:12:15 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import './styles/tailwind.css'
 import './animations/scaledown.css'
 import './animations/gradient.css'
 import Cards from './components/cards';
+import instagram from './images/instagram.svg'
+import linkedin from './images/linkedin.svg'
+import youtube from './images/youtube.svg'
+import telegram from './images/telegram.svg'
+import facebook from './images/facebook.svg'
 
 function App() {
   return (
@@ -18,9 +23,20 @@ function App() {
           <img src={Navlogo} alt='Xponent' className='scale-down-center md:animate-none size-12/12 md:size-2/12 ' />
         </div>
 
-        <div className='flex justify-center'>
-
-          <Cards />
+        <div className='flex justify-center mb-4'>
+          <Cards icon={instagram} logoUrl='//instagram.com/_u/xponentfunds/#Intent;package=com.instagram.android;scheme=https;end' text='Checkout our Instagram Page!' textUrlDisplay='@xponentfunds' />
+        </div>
+        <div className='flex justify-center mb-4'>
+          <Cards icon={facebook} logoUrl='https://www.facebook.com/xponentfunds' text='Follow us on Facebook!' textUrlDisplay='fb/xponentfunds' />
+        </div>
+        <div className='flex justify-center mb-4'>
+          <Cards icon={youtube} logoUrl='//instagram.com/_u/xponentfunds/#Intent;package=com.instagram.android;scheme=https;end' text='Checkout our Instagram Page!' textUrlDisplay='@xponentfunds' />
+        </div>
+        <div className='flex justify-center mb-4'>
+          <Cards icon={telegram} logoUrl='//instagram.com/_u/xponentfunds/#Intent;package=com.instagram.android;scheme=https;end' text='Checkout our Instagram Page!' textUrlDisplay='@xponentfunds' />
+        </div>
+        <div className='flex justify-center mb-4'>
+          <Cards icon={linkedin} logoUrl='//instagram.com/_u/xponentfunds/#Intent;package=com.instagram.android;scheme=https;end' text='Checkout our Instagram Page!' textUrlDisplay='@xponentfunds' />
         </div>
 
       </div>
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,26 +1,22 @@
 import React from "react";
 import Navlogo from '../images/navlogo.svg'
-import instagram from '../images/instagram.svg'
-import linkedin from '../images/linkedin.svg'
-import youtube from '../images/youtube.svg'
-import telegram from '../images/telegram.svg'
 import '../styles/tailwind.css'
 import '../animations/scaledown.css'
 import '../animations/gradient.css'
 import '../animations/click.css'
 
-const Cards = () => {
+const Cards = ({logoUrl, textUrl, icon, text, textUrlDisplay}) => {
     return (
         <div className='flex content-center items-center justify-start rounded-xl gradient-background drop-shadow-md' style={{ height: '20vh', width: '80vw' }}>
-            <div className="flex-row justify-center">
-                <a href="intent://instagram.com/_u/xponentfunds/#Intent;package=com.instagram.android;scheme=https;end">
+            <div className="flex-row gap-2 justify-center">
+            <a href={logoUrl}>
 
-                    <img src={instagram} className='bg-white/10 rounded-full clickable-element m-5 size-20 md shadow-2xl' alt='' style={{}}></img>
+                    <img src={icon} className='bg-white/10 rounded-full clickable-element m-5 size-20 md shadow-2xl' alt='' style={{}}></img>
                 </a>
             </div>
             <div>
-                <p className="text-xl text-center text-white">Check out our Instagram!</p>
-                <a className="text-2xl text-white font-bold" href='https://www.instagram.com/xponentfunds'>@xponentfunds</a>
+                <p className="text-sm md:text-xl text-center text-white">{text}</p>
+                <a className="text-xl md:text-2xl text-white font-bold" href={textUrl}>{textUrlDisplay}</a>
             </div>
             
         </div>
