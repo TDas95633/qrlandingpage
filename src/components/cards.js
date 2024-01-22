0fc6412142eb741523db9188a60e0f281d6cb987
Tuhin Das <dastuhin.rnj@gmail.com>
2024-01-22 16:38:42 +0530

    updare

--- src/components/cards.js
+++ src/components/cards.js
@@ -10,7 +10,7 @@ const Cards = () => {
     return (
         <div className='flex content-center items-center justify-start rounded-xl gradient-background drop-shadow-md' style={{ height: '20vh', width: '80vw' }}>
             <div className="flex-row justify-center">
-                <a href="intent://instagram.com/_u/ebadgh/#Intent;package=com.instagram.android;scheme=https;end">
+                <a href="intent://instagram.com/_u/xponentfunds/#Intent;package=com.instagram.android;scheme=https;end">
 
                     <img src={instagram} className='bg-white/10 rounded-full clickable-element m-5 size-20 md shadow-2xl' alt='' style={{}}></img>
                 </a>
