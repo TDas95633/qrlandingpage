614cdbb7892f8c09c1c8add1599391f4cbd51fd9
Tuhin Das <dastuhin.rnj@gmail.com>
2024-01-24 00:40:16 +0530

    1

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ function App() {
           <Cards icon={linkedin} logoUrl='https://www.linkedin.com/company/xponent-fund-management' text='Connect with us!' textUrlDisplay='Xponent Funds' textUrl='https://www.linkedin.com/company/xponent-fund-management' />
         </div>
         <div className='flex justify-center mb-4'>
-          <Cards icon={email} logoUrl='mailto:help@xponentfunds.com' text='Reach out to us via' textUrlDisplay='help@xponentfunds.com' textUrl='mailto:help@xponentfunds.com' customPadding='p-3' />
+          <Cards icon={email} logoUrl='mailto:help@xponentfunds.com' text='Reach out to us via' textUrlDisplay='help@xponentfunds.com' textUrl='mailto:help@xponentfunds.com' customPadding='p-2' />
         </div>
         <div className='flex justify-center pb-8'>
-          <Cards icon={call} logoUrl='tel:+911169290876' text='Just a single call away!' textUrlDisplay='116 929 0876' textUrl='tel:+911169290876' customPadding='p-3' />
+          <Cards icon={call} logoUrl='tel:+911169290876' text='Just a single call away!' textUrlDisplay='116 929 0876' textUrl='tel:+911169290876' customPadding='p-2' />
         </div>
 
       </div>
